5d5fa95d5878c51a5c4e5838154b30add121aeaf
Luke Cahill <lmc@outlook.com>
2016-10-03 20:07:32 +0100

    Add new aout route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lukes Node Site' });
+});
+
+router.get('/about', function(req, res) {
+  res.render('about', {
+    title: 'About'
+  });
 });
 
 module.exports = router;
