4c813f2b4bccab1048847f2a9a4ddc6bc2f942c8
Luke Cahill <lmc@outlook.com>
2016-10-05 21:45:41 +0100

    Add MongoDb read

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var database;
+MongoClient.connect('mongodb://localhost:27017/shop_database', function(err, db) {
+  if(!err) {
+    console.log('Connected to MongoDb');
+    database = db;
+  }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lukes Node Site' });
@@ -10,6 +19,32 @@ router.get('/about', function(req, res) {
   res.render('about', {
     title: 'About'
   });
+});
+
+router.get('/books', function(req, res) {
+  res.render('books', {
+    title: 'All books'
+  })
+}); 
+
+router.get('/authors', function(req, res) {
+  var collection = database.collection('books');
+  var items = collection.find();
+  items.each(function(err, i) {
+    if(i == null) {
+      database.close();
+      return;
+    }
+    console.log(i.Author)
+  });
+  
+  database.close();
+  res.render('authors');
+}); 
+
+router.post('/authors', function(req, res) {
+  console.log(req);
+  console.log(res);
 });
 
 module.exports = router;
