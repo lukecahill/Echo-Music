597a3dd908477793eb95f113f35f7f1ea5696474
Luke Cahill <lmc@outlook.com>
2016-10-15 11:50:49 +0100

    Include requires for socket.io usage

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var users = require('./routes/users');
 var app = express();
 var MongoClient = require('mongodb');
 
+var server = require('http').createServer(express);
+var io = require('socket.io')(server);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use('/javascripts', express.static('./javascripts'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
