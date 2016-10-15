7c3d25adf823aede9b26e2b28218716ac9b4064a
Luke Cahill <lmc@outlook.com>
2016-10-15 13:27:03 +0100

    Add functionality to the chat, update minified JS

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,15 +1,38 @@
 var socket = io.connect('http://localhost:8080');
 
 (function() {
+    var lastMessage = 0, $welcome = $('.welcome'), $chatbox = $('#chatbox'), $name = $('#name');
+    var $message = $('#message');
+
+    function SendMessage() {
+        socket.emit('chat message', $message.val());
+        $message.val('');
+    }
+
+    function SetName() {
+        var setName = $.trim($name.val());
+        if($name !== '') {
+            $welcome.html('Welcome, <b>' + setName + '</b>');
+            $('#setYourName').hide();
+            $welcome.show();
+        } else {
+            alert('Your name cannot be blank!');
+        }
+    }
+
+    $message.on('keyup', function(event) {
+        if(event.keyCode == 13) {
+            SendMessage();
+        }
+    })
+
     $('#sendMessage').on('click', function() {
-        socket.emit('chat message', $('#message').val());
-        $('#message').val('');
-        return false;
+        SendMessage();
     });
 
     socket.on('chat message', function(msg) {
         console.log(msg)
-        var m = '<p><span class="sender" ' + msg + '</span>: ' + msg + '<hr /></p>';
-        $('#chatbox').append(m);
+        var m = '<p><span class="sender" ' + $name.val() + '</span>: ' + msg + '<hr /></p>';
+        $chatbox.append(m);
     });
 })();
