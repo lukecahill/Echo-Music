7376b2b03863059324d2ef35ff2d60eec2b76a43
Luke Cahill <lmc@outlook.com>
2016-10-16 21:26:46 +0100

    Add users get for authentication

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var bodyParser = require('body-parser');var express = require('express');
 var router = express.Router();
+
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var config = require('../config.js');
+var User = require('../models/user.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    User.find({}, function(err, users) {
+        res.json(users);
+    })
+     //res.send('respond with a resource');
 });
 
 module.exports = router;
