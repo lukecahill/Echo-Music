c92bf45302298f780973403ec0a74c0d96acf5ad
Luke Cahill <lmc@outlook.com>
2016-10-16 22:16:24 +0100

    Add the login route for users to log in with

---
+++ routes/login.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var config = require('../config.js');
+var User = require('../models/user.js');
+
+router.get('/', function(req, res) {
+    res.render('login');
+});
+
+module.exports = router;
